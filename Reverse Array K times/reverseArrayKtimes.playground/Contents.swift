1cc10e4d4e92c8b221e0367c340e24545f23c6a4
Ananth <agirijak@in.ibm.com>
2018-12-19 00:00:43 +0530

    added reverse array k times code

--- Reverse Array K times/reverseArrayKtimes.playground/Contents.swift
+++ Reverse Array K times/reverseArrayKtimes.playground/Contents.swift
@@ -3,11 +3,17 @@ import UIKit
 var a = [1,2,3,4,5,6,7]
 
 
+
+/**
+ 
+ Logic is to create two ArraySlices from the given array . One slice is from 0 to given postion and another from position to last element. Then append the first slice to the last of second slice.
+ */
 func rotate(a:[Int], n:Int, d:Int) -> [Int]{
     
     if d == 0 || d == n {
         return a
     }
+    
     return Array(a[d..<n] + a[0..<d])
 }
 
